761722dd1eda09c104b5c1e5665472d014a4ec69
Vellanueva <838884524@qq.com>
2019-12-25 17:07:07 +0800

    feat: scheduled task

--- controller/src/main/java/com/ssq/infrastructure/ControllerApplication.java
+++ controller/src/main/java/com/ssq/infrastructure/ControllerApplication.java
@@ -2,11 +2,13 @@ package com.ssq.infrastructure;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * 入口
  */
 @SpringBootApplication
+@EnableScheduling
 public class ControllerApplication {
 
 	public static void main(String[] args) {
---
+++ controller/src/main/java/com/ssq/infrastructure/ScheduledTask.java
@@ -0,0 +1,22 @@
+package com.ssq.infrastructure;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @author sunsiquan
+ * @date 2019-12-25
+ */
+@Component
+public class ScheduledTask {
+
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+    @Scheduled(fixedRate = 5000)
+    public void reportCurrentTime() {
+        System.out.println("现在时间是" + dateFormat.format(new Date()));
+    }
+}
